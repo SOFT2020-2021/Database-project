d01c8e76ea0747a7b52d6d87cb54a80eba0ff67a
JonasGroenbek <jonasgroenbek@gmail.com>
2020-05-26 10:20:04 +0200

    _

--- config.js
+++ config.js
@@ -1,20 +1,15 @@
-<<<<<<< HEAD
 const production = true
 const pokemonLimit = production ? 1000 : 2
+const tables = Object.freeze({
+    tableName: 'Pokemon',
+})
+const logTypes = Object.freeze({
+    BATTLE: 'battle',
+})
 
 module.exports = {
     pokemonLimit,
-    production
+    production,
+    tables,
+    logTypes,
 }
-=======
-module.exports.tables = Object.freeze({
-    tableName: "Pokemon"
-})
-
-module.exports.pokemonLimit = 1000
-
-
-module.exports.logTypes = Object.freeze({
-    BATTLE: "battle"
-})
->>>>>>> 1d93fb771e3a2e8584a0a6835ba37741ddb74b63
